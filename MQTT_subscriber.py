c859086ccacf5376e27f05859c56b0be543fea8b
Richard Kuo <rkuo2000@gmail.com>
2025-09-18 06:16:11 +0800

    Delete MQTT_subscriber.py

--- MQTT_subscriber.py
+++
@@ -1,4 +0,0 @@
-import paho.mqtt.subscribe as subscribe
-host = "test.mosquitto.org"
-msg = subscribe.simple("TCFST/Robot1", hostname=host)
-print("%s %s" % (msg.topic, msg.payload.decode("utf-8")))
