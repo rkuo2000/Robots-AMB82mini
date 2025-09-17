e2bca1497cca0623887356a0792d6ee49cd2b288
Richard Kuo <rkuo2000@gmail.com>
2025-09-18 07:48:47 +0800

    Add files via upload

---
+++ AMB82-mini/GenAIVision/GenAIVision.ino
@@ -0,0 +1,109 @@
+/*
+
+This sketch shows the example of image prompts using APIs.
+
+openAI platform - openAI vision
+https://platform.openai.com/docs/guides/vision
+
+Google AI Studio - Gemini vision
+https://ai.google.dev/gemini-api/docs/vision
+
+GroqCloud - Llama vision
+https://console.groq.com/docs/overview
+
+Example Guide: https://ameba-arduino-doc.readthedocs.io/en/latest/amebapro2/Example_Guides/Neural%20Network/Generative%20AI%20Vision.html
+
+Credit : ChungYi Fu (Kaohsiung, Taiwan)
+
+*/
+
+String openAI_key = "";               // paste your generated openAI API key here
+String Gemini_key = "";               // paste your generated Gemini API key here
+String Llama_key = "";                // paste your generated Llama API key here
+char wifi_ssid[] = "TCFSTWIFI.ALL";    // your network SSID (name)
+char wifi_pass[] = "035623116";        // your network password
+
+#include <WiFi.h>
+#include "GenAI.h"
+#include "VideoStream.h"
+WiFiSSLClient client;
+GenAI llm;
+VideoSetting config(768, 768, CAM_FPS, VIDEO_JPEG, 1);
+#define CHANNEL 0
+
+uint32_t img_addr = 0;
+uint32_t img_len = 0;
+
+String prompt_msg = "Please describe the image, and if there is a text, please summarize the content";
+
+const int buttonPin = 1;          // the number of the pushbutton pin
+
+void initWiFi()
+{
+    for (int i = 0; i < 2; i++) {
+        WiFi.begin(wifi_ssid, wifi_pass);
+
+        delay(1000);
+        Serial.println("");
+        Serial.print("Connecting to ");
+        Serial.println(wifi_ssid);
+
+        uint32_t StartTime = millis();
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            if ((StartTime + 5000) < millis()) {
+                break;
+            }
+        }
+
+        if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("");
+            Serial.println("STAIP address: ");
+            Serial.println(WiFi.localIP());
+            Serial.println("");
+            break;
+        }
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    initWiFi();
+
+    config.setRotation(0);
+    Camera.configVideoChannel(CHANNEL, config);
+    Camera.videoInit();
+    Camera.channelBegin(CHANNEL);
+    Camera.printInfo();
+    
+    pinMode(buttonPin, INPUT);
+    pinMode(LED_B, OUTPUT);
+    pinMode(LED_G, OUTPUT);    
+}
+
+void loop()
+{
+     if ((digitalRead(buttonPin)) == 1) {
+        // Start MP4 recording after 3 seconds of blinking
+        for (int count = 0; count < 3; count++) {
+            digitalWrite(LED_B, HIGH);
+            delay(500);
+            digitalWrite(LED_B, LOW);
+            delay(500);
+        }
+
+    // Vision prompt using same taken image
+       Camera.getImage(0, &img_addr, &img_len);        
+    // openAI vision prompt
+    //  String text = llm.openaivision(openAI_key, "gpt-4o-mini", prompt_msg, img_addr, img_len, client);
+
+    // Gemini vision prompt        
+        String text = llm.geminivision(Gemini_key, "gemini-2.0-flash", prompt_msg, img_addr, img_len, client);
+
+    // Llama vision prompt
+    //  String text = llm.llamavision(Llama_key, "llama-3.2-90b-vision-preview", prompt_msg, img_addr, img_len, client); 
+        Serial.println(text);
+    }
+}
